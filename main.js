701352c9cfb4a9d1f47d88135a4b9c1df7894885
Rosieee <baring.jp@obmontessori.edu.ph>
2025-03-11 19:09:04 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,40 +2,43 @@ document.addEventListener("DOMContentLoaded", function () {
     function toggleVisibility(showElement, hideElements) {
         document.getElementById(showElement).classList.remove("d-none");
         hideElements.forEach(element => {
-            document.getElementById(element).classList.add("d-none");
+            let el = document.getElementById(element);
+            if (el) el.classList.add("d-none");
         });
     }
 
     function hideFareGuide() {
-        document.getElementById("fare-guide").classList.add("d-none");
+        let fareGuide = document.getElementById("fare-guide");
+        if (fareGuide) fareGuide.classList.add("d-none");
     }
 
     function showFareGuide() {
-        document.getElementById("fare-guide").classList.remove("d-none");
+        let fareGuide = document.getElementById("fare-guide");
+        if (fareGuide) fareGuide.classList.remove("d-none");
     }
 
     document.getElementById("transitScheduleBtn").addEventListener("click", function () {
-        toggleVisibility("transit-schedule", ["contact-us", "homepage"]);
+        toggleVisibility("transit-schedule", ["contact-us", "fare-guide"]);
         hideFareGuide();
     });
 
     document.getElementById("contactUsBtn").addEventListener("click", function () {
-        toggleVisibility("contact-us", ["transit-schedule", "homepage"]);
+        toggleVisibility("contact-us", ["transit-schedule", "fare-guide"]);
         hideFareGuide();
     });
 
     document.getElementById("homeBtn").addEventListener("click", function () {
-        toggleVisibility("homepage", ["transit-schedule", "contact-us"]);
+        toggleVisibility("fare-guide", ["transit-schedule", "contact-us"]);
         showFareGuide();
     });
 
     document.getElementById("closeScheduleBtn").addEventListener("click", function () {
-        toggleVisibility("homepage", ["transit-schedule", "contact-us"]);
+        toggleVisibility("fare-guide", ["transit-schedule", "contact-us"]);
         showFareGuide();
     });
 
     document.getElementById("closeContactBtn").addEventListener("click", function () {
-        toggleVisibility("homepage", ["transit-schedule", "contact-us"]);
+        toggleVisibility("fare-guide", ["transit-schedule", "contact-us"]);
         showFareGuide();
     });
 });
